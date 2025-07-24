31ed0c17533d8faf8c3cc1544b757249b324c504
Hangning Zhou <hnzhou16@outlook.com>
2025-07-24 16:18:20 -0700

    update logo

--- src/components/consts.js
+++ src/components/consts.js
@@ -1,5 +1,4 @@
 export const serverAPI = process.env.NODE_ENV === "production" ? process.env.REACT_APP_DOMAIN_SERVER : process.env.REACT_APP_LOCAL_SERVER
-// export const serverAPI = process.env.REACT_APP_LOCAL_SERVER
 export const fetchFilterUrl = `${serverAPI}/filter/getFilter`
 export const fetchProductsUrl = `${serverAPI}/filter/filteredProducts`
 export const singleProductUrl = `${serverAPI}/product`
